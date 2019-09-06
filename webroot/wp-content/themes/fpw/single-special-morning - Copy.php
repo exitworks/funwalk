24c710808f1636a6fc17dcf22a2fd7bbebaea1e4
exitworks <deguchi@exitworks.co.jp>
2019-09-06 13:00:59 +0900

    月間

---
+++ webroot/wp-content/themes/fpw/single-special-morning - Copy.php
@@ -0,0 +1,192 @@
+<?php get_header(); ?>
+
+<div class="container" style="">
+	<div class="breadcrumb"><a href="/">FUN+WALK PROJECTトップ</a>　&gt;　<a href="/<?php echo esc_html($post_type);?>/">SPECIAL</a>　&gt;
+		<?php the_title(); ?>
+	</div>
+	<div class="contents">
+		<!--============ main =================-->
+		<main id="col1">
+			<style>
+				.contents {
+					max-width: 100% !important;
+					overflow: auto !important;
+				}
+			</style>
+			
+			<div id="morning">
+				<div class="morning-main"> <img src="/wp-content/themes/fpw/assets/img/morning/morning_main.jpg" alt=""/> </div>
+				
+				
+				
+				
+				<div class="morning-inner">
+
+					<h2 class="morning-ttl">朝食WALKING SPECIAL ROAD</h2>
+					
+					<h3>10月○日～10月〇日の期間限定で<br class="spBox">池尻大橋駅～渋谷駅間を歩いて<br>
+健康的なカラダと<br class="spBox">参加特典をゲットしよう！</h3>
+					
+					<p class="m-txt01">参加するごとに参加特典として<br>
+健康に楽しく歩くために必要なドリンクや<br class="spBox">糖質（フルーツ）を配布</p>
+
+<p class="m-txt01">Goal-Spotでは○○のパンとEXEIL TETSUYAさん<br class="spBox">プロデュースのコーヒーを配布</p>
+
+<p class="m-txt01">更に全○日間参加の皆勤ウォーカーの方で応募して<br>
+抽選で選ばれた方にFUN+WALK特製スニーカーを<br class="spBox">プレゼント！</p>
+
+					<div class="m-map">
+				    <div class="pcBox"><img src="/wp-content/themes/fpw/assets/img/morning/map.png" alt=""/></div>
+					<div class="spBox"><img src="/wp-content/themes/fpw/assets/img/morning/map_sp.png" alt=""/></div> 	
+					</div>
+					
+					
+					
+					<h2 class="morning-ttl">朝食WALKING COUPON</h2>
+					
+					<h3>毎朝歩いて、ポイントをためて、<br class="spBox">割引クーポンをゲット！</h3>
+					
+					<p class="m-txt01">10月1日～10月31日の期間限定FUN+WALKアプリの歩いて貯まるポイントで、<br>
+渋谷、池尻大橋、中目黒周辺の飲食店の「朝or昼に使えるクーポン＋アサヒ飲料製品1本」をゲットしよう！<br>
+毎日～12時までに〇ポイント貯めた方は期間限定の<br class="spBox">割引クーポンをプレゼント！<br>
+詳しくはFUN+WALKアプリをチェック！</p>
+					
+					<div class="m-boxA">
+					<h2 class="morning-ttl">参加フロー</h2>
+					<div class="m-boxB">
+						<div class="leftBoxA"><span>1</span></div>
+						<div class="rightBoxA">
+						<p class="indent"><strong>●参加前に「FUN+WALK」アプリをインストールしよう！</strong></p>
+						<p class="indent"><strong>●参加特典のアサヒ飲料のドリンクと軽食を摂取してから歩き始めよう！</strong></p>
+							<p class="indent"><span class="small">※ウォーキング後は食欲が旺盛になってしまう為、ウォーキング前に最低限の糖質を摂取する必要がある。</span></p>
+						</div>
+						</div>
+					
+						
+					<div class="m-boxB">
+						<div class="leftBoxA"><span>2</span></div>
+						<div class="rightBoxA">
+						<p class="indent"><strong>●Goalの受付にて、「FUN＋WALK」アプリにて本日の歩数を提示して、○○歩以上の方はスタンプを押す。</strong></p>
+						<p class="indent"><span class="small">※「FUN＋WALK」アプリをインストールしていない方は、その他歩数が確認できるアプリで歩数をご提示下さい</span></p>
+						<br>
+	
+						<p class="indent"><strong>●スタンプをもらった方にはアサヒ飲料のドリンクと○○のパン、EXILEのTETSUYAさんプロデュースのコーヒーをプレゼント！</strong></p>
+							<p class="indent"><span class="small">※ウォーキング後はエネルギーが不足している可能性があるため、糖質を摂取する必要がある。</span></p>	
+						</div>
+						</div>
+						
+						
+						
+					<div class="m-boxB">
+						<div class="leftBoxA"><span>3</span></div>
+						<div class="rightBoxA">
+						<p class="indent"><strong>●全○日間参加した皆勤ウォーカーには応募した人の中から抽選で選ばれた方に特製シューズをプレゼント！</strong></p>
+						<p class="indent"><strong>●皆勤ウォーカーの方は受付にてQRコードを読み取り、応募フォームを入力していただく。</strong></p>
+						<p class="indent"><strong>●当選者の方には発送にて当選発表とさせていただく。</strong></p>
+						</div>
+						</div>	
+						
+						
+						
+						
+						
+					
+					</div>
+					
+					<div class="m-boxA">
+					<h2 class="morning-ttl">参加賞</h2>
+					<h3>毎朝歩いて、ポイントをためて、割引クーポンをゲット！</h3>
+					
+					<div class="m-boxC">
+						<div class="leftBoxB"><img src="/wp-content/themes/fpw/assets/img/morning/s01.png" width="399" height="253" alt=""/></div>
+						
+						<div class="rightBoxB">
+						<div class="txt02">Start Spot プレゼント</div>
+						<p>・アサヒ十六茶 orアサヒ からだ十六茶<br>
+・軽食<br>
+・ノベルティバッグ<br>
+・リーフレット</p>
+						
+						</div>
+						
+					  </div>
+						
+						
+						
+					<div class="m-boxC">
+						<div class="leftBoxB"><img src="/wp-content/themes/fpw/assets/img/morning/s01.png" width="399" height="253" alt=""/></div>
+						
+						<div class="rightBoxB">
+						<div class="txt02">Goal Spot プレゼント</div>
+						<p>・アサヒ飲料のドリンク<br>
+・軽食</p>
+						
+						</div>
+						
+					  </div>	
+						
+						
+						
+					
+					<h2 class="morning-ttl mt50">ゴールドプライズ</h2>
+					<h3>さらに、3日間参加し、スタンプを3つ獲得した皆勤ウォーカーの方には<br>
+「ゴールドプライズ」プレゼントの抽選に応募できます！</h3>	
+						
+						<div class="m-boxC">
+						<div class="leftBoxB"><img src="/wp-content/themes/fpw/assets/img/morning/s01.png" width="399" height="253" alt=""/></div>
+						
+						<div class="rightBoxB">
+						<div class="txt02">商品名商品名商品名商品名</div>
+						<p>抽選で30名様にプレゼント！<br>
+スタンプ3つ目指してがんばろう！</p>
+						
+						</div>
+						
+					  </div>	
+						
+						
+					</div>
+					
+					
+					<div class="m-boxA">
+					<h2 class="morning-ttl">#funpluswalkをつけて○○で<br>
+投稿しよう！</h2>
+					<h3>サコッシュ＋からだ十六茶１ケース／抽選で２０名様<br>
+特製スニーカー／<br class="spBox">抽選で２０名様が当たる！</h3>	
+					
+						<h4>期間</h4>
+						<p>2019年10月1日～10月31日</p>
+						
+						<h4>投稿条件</h4>
+						<p class="m-boxD">①「#funpluswalk」(or「＃ファンプラスウォーク」)と「#朝食WALKING」「#朝食ウォーキング」
+</p>
+						<div class="m-icon">+</div>
+						<p class="m-boxD">②下記いずれかの写真を投稿<br>
+「朝食WALKINGしている様子」（歩いている様子、足元（スニーカーなど）の画像やFUN+WALKアプリ画面でも可能）
+
+
+</p>
+						
+						<h4>抽選でいずれかをプレゼント</h4>
+					<p>サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト</p>
+					</div>
+					
+					
+					
+					
+			  </div>
+				
+				
+			</div>
+			
+			
+			
+		</main>
+		<!--============ /main =================-->
+
+
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
---
+++ webroot/wp-content/themes/fpw/single-special-morning.php
@@ -0,0 +1,140 @@
+<?php get_header(); ?>
+
+<div class="container" style="">
+	<div class="breadcrumb"><a href="/">FUN+WALK PROJECTトップ</a>　&gt;　<a href="/<?php echo esc_html($post_type);?>/">SPECIAL</a>　&gt;
+		<?php the_title(); ?>
+	</div>
+	<div class="contents">
+		<!--============ main =================-->
+		<main id="col1">
+			<style>
+				.contents {
+					max-width: 100% !important;
+					overflow: auto !important;
+				}
+			</style>
+			
+			<div id="morning">
+				<div class="morning-main"> <img src="/wp-content/themes/fpw/assets/img/morning/morning_main.jpg" alt=""/> </div>
+				
+				
+				
+				
+				<div class="morning-inner">
+
+				  <h2 class="morning-ttl">10月1日(火)より2019年度の強化月間<br class="spBox">「FUN+WALK月間」がスタート！</h2>
+					
+				  <h3>「歩く」ことへの全国的な機運を<br class="spBox">一層高める「FUN＋WALK月間」、<br>
+東京都内では「朝食」と<br class="spBox">「歩く」を掛け合わせ<br>
+“脳と体の活性化”を促すイベントを実施。
+</h3>
+					
+				  <p class="m-txt01">期間中は東京都内渋谷、池尻大橋、中目黒などの地域にて、企業の健康経営を積極的に推進するアサヒ飲料株式会社、東急株式会社、アシックスジャパン株式会社と連携し、ビジネスパーソンをメインターゲットとした取り組み「FUN+WALK MORNING」を開催します。特に、3日間限定で実施する「FUN+WALK MORNING」スペシャルデーでは、働き方改革関連法案の施行後初めて迎える強化月間として、朝食とウォーキングの掛け算を楽しんでいただきながら、ビジネスパーソンの労働生産性向上も期待できる“脳と体の活性化”を促進していきます。
+</p>
+
+<div class="morning-btn"><a href="/special/app/"><img src="/wp-content/themes/fpw/assets/img/morning/btn_app.png" alt=""/></a></div>
+					
+					
+					
+					
+			
+					
+					
+					
+					
+					
+					
+					<div class="m-boxA">
+					
+					<h2 class="morning-ttl">「FUN+WALK MORNING」<br class="spBox">について</h2>
+					
+					<h3>「FUN+WALK MORNING」<br class="spBox">スペシャルデー [荒天中止]<br>
+<span>10月15日(火)～10月17日(木)の3日間限定</span></h3>
+					
+					<p class="m-txt01">「FUN+WALK MORNING」のスペシャルコンテンツとして、3日間限定で、東急田園都市線「池尻大橋駅」～「渋谷駅」間をメインコースにウォーキングイベントを開催。脳と体を活性化するために必要なドリンクやフルーツに加え、ウォーキング後の朝食、限定ノベルティを無料で配布するなど、より楽しんでご参加いただける仕組みをご用意します。 </p>
+					
+					
+					
+					<div class="m-map">
+				    <div class="pcBox"><img src="/wp-content/themes/fpw/assets/img/morning/map.png" alt=""/></div>
+					<div class="spBox"><img src="/wp-content/themes/fpw/assets/img/morning/map_sp.png" alt=""/></div> 	
+					</div>
+					<div><img src="/wp-content/themes/fpw/assets/img/morning/txt01.png" alt="" class="pcBox"/>
+						<img src="/wp-content/themes/fpw/assets/img/morning/txt01_sp.png" alt="" class="spBox"/>
+						
+					  </div>	
+					  <p class="m-txt02">※中止の場合はFUN+WALK PROJECTのHP等でお知らせします。 ※スタート地点およびゴール地点での配布物は異なります。　※ゴール地点では、スタート地点で配布するチラシをお待ちの方、もしくは3,000歩あるいたことが証明された方にのみ配布物をお渡しいたします。なお、それぞれの配布物の数には限りがございます。先着順でのお渡しとなります。</p>
+						
+						
+					<h3>「FUN+WALK MORNING」クーポン<br>
+<span>2019年10月1日(火)～10月31日(木)</span></h3>
+						
+					  <div class="appBox">
+				<div class="app-left">
+            <div class="pcBox"><img src="/wp-content/themes/fpw/assets/img/app/app_img01.png" width="262" height="554"></div>
+            <div class="spBox"><img src="/wp-content/themes/fpw/assets/img/app/app_img01_sp.png" width="262" height="554"></div>
+          </div>
+          <div class="app-right">
+           <p class="m-txt01">「FUN+WALKアプリ」を使用して歩くと貯まるポイントを引き換えに参加できるお得なキャンペーン。渋谷・池尻大橋・中目黒周辺の飲食店で、朝の通勤時間帯～ランチタイムの時間帯限定で使えるクーポンとアサヒ飲料の製品1本がもらえます。 </p>	
+            
+            <p class="app-attention">スマートフォンの画面を見つめながらの移動は大変危険です。また自分自身だけでなく、周囲の方も巻き込む事故につながる可能性があります。<br />
+              歩行中はもちろん、車や自転車の運転中は、本アプリのご利用をお控えください。</p>
+            <div class="qrBox">
+              <div class="qrInner">
+                <div class="qr-ttl">早速インストールしよう</div>
+                <div class="qr-left">
+                  <div class="qr-ttl02">Androidの方</div>
+                  <img src="/wp-content/themes/fpw/assets/img/app/app_qr01.png" width="139" height="139">
+                  <div class="qr-btn"><a href="https://funpluswalk.jp/qr/Android.html" target="_blank"><img src="/wp-content/themes/fpw/assets/img/app/app_btn_google.png"></a></div>
+                </div>
+                <div class="qr-right">
+                  <div class="qr-ttl02">iPhoneの方</div>
+                  <img src="/wp-content/themes/fpw/assets/img/app/app_qr02.png" width="139" height="139">
+                  <div class="qr-btn"><a href="https://funpluswalk.jp/qr/iOS.html" target="_blank"><img src="/wp-content/themes/fpw/assets/img/app/app_btn_apple.png"></a></div>
+                </div>
+              </div>
+            </div>		
+			 </div>				
+				</div>			
+						
+						
+						
+						
+						
+					
+						
+						
+						
+						
+					
+					  <h4>期間</h4>
+					  <p>2019年10月1日～10月31日</p>
+						
+					  <h4>投稿条件</h4>
+					  <p><a href="/special/app/">「FUN＋WALKアプリ」をダウンロード</a>して、各店舗のクーポンをゲットしてください。<br>
+8,000歩ごとにクーポンがゲットできます。</p>
+
+					</div>
+					
+					
+					<div class="morning-footer"><a href="/"><img src="/wp-content/themes/fpw/assets/img/morning/morning_img01.png" alt=""/></a><a href="#"><img src="/wp-content/themes/fpw/assets/img/morning/morning_img02.png" alt=""/></a><a href="http://www.mext.go.jp/sports/" target="_blank"><img src="/wp-content/themes/fpw/assets/img/morning/morning_img03.png" alt=""/></a><br class="spBox">
+<img src="/wp-content/themes/fpw/assets/img/morning/morning_img04.png" alt=""/><a href="https://www.city.shibuya.tokyo.jp/" target="_blank"><img src="/wp-content/themes/fpw/assets/img/morning/morning_img05.png" alt=""/></a><br class="spBox"><img src="/wp-content/themes/fpw/assets/img/morning/morning_img06.png" alt=""/><a href="https://www.city.shibuya.tokyo.jp/" target="_blank"><img src="/wp-content/themes/fpw/assets/img/morning/morning_img07.png" alt=""/></a><a href="https://www.asics.com/jp/ja-jp/" target="_blank"><img src="/wp-content/themes/fpw/assets/img/morning/morning_img08.png" alt=""/></a><a href="https://www.tokyu.co.jp/index.html" target="_blank"><img src="/wp-content/themes/fpw/assets/img/morning/morning_img09.png" alt=""/></a></div>
+					
+				  <h3>詳細はこのサイトで随時お知らせします!</h3>
+					
+			  </div>
+				
+				
+			</div>
+			
+			
+			
+		</main>
+		<!--============ /main =================-->
+
+
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
